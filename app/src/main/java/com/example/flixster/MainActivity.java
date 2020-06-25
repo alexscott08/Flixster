9ed466a8de3e76de8927c946896b5b64181319b3
Alex Scott <axscott@fb.com>
2020-06-25 15:32:57 -0400

    Imported ActivityMainBinding class using View Binding, to reduce boilerplate.

--- app/src/main/java/com/example/flixster/MainActivity.java
+++ app/src/main/java/com/example/flixster/MainActivity.java
@@ -6,10 +6,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.example.flixster.adapters.MovieAdapter;
+import com.example.flixster.databinding.ActivityMainBinding;
 import com.example.flixster.models.Movie;
 
 import org.json.*;
@@ -27,17 +29,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //use view binder
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
         RecyclerView rvMovies = findViewById(R.id.rvMovies);
         movies = new ArrayList<>();
 
-        //Create the adapter
+        // Create the adapter
         final MovieAdapter movieAdapter = new MovieAdapter(this, movies);
 
-        //Set the adapter on the recycler view
+        // Set the adapter on the recycler view
         rvMovies.setAdapter(movieAdapter);
 
-        //Set a Layout Manager on the recycler view
+        // Set a Layout Manager on the recycler view
         rvMovies.setLayoutManager(new LinearLayoutManager(this));
 
         AsyncHttpClient client = new AsyncHttpClient();
